0ec10cbd50c1ff649905747a282da7faa7f5633c
Zack Koppert <zkoppert@github.com>
2020-11-11 09:15:55 -0800

    Add isEvenEnd func
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- even-end.go
+++ even-end.go
@@ -5,13 +5,21 @@ import (
 )
 
 func main() {
-	for i := 1000; i <= 1010; i++ {
-		isEvenEnd(i)
+	var i int
+	for i = 1; i <= 35; i++ {
+		if isEvenEnd(i) {
+			fmt.Printf("%d is even-ended\n", i)
+		} else {
+			fmt.Printf("%d is not even-ended\n", i)
+		}
 	}
 }
 
-func isEvenEnd(x int) {
-	fmt.Printf("%x is even-ended")
-	fmt.Printf("%x is not even-ended")
+func isEvenEnd(x int) bool {
+	xString := fmt.Sprintf("%d", x)
 
+	if xString[0] == xString[len(xString)-1] {
+		return true
+	}
+	return false
 }
