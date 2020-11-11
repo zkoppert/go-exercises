0867170b175107f3fe36c6b120cac01e70bc4016
Zack Koppert <zkoppert@github.com>
2020-11-11 09:38:02 -0800

    Switch to multiplying
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- even-end/even-end.go
+++ even-end/even-end.go
@@ -5,17 +5,19 @@ import (
 )
 
 func main() {
-	var i int
-	var count int
+	var i, j int
+	count := 0
 	start := 1000
 	end := 9999
 
 	for i = start; i <= end; i++ {
-		if isEvenEnd(i) {
-			//fmt.Printf("%d is even-ended\n", i)
-			count++
-		} else {
-			//fmt.Printf("%d is not even-ended\n", i)
+		for j = i; j <= end; j++ {
+			if isEvenEnd(i * j) {
+				//fmt.Printf("%d is even-ended\n", i)
+				count++
+			} else {
+				//fmt.Printf("%d is not even-ended\n", i)
+			}
 		}
 	}
 	fmt.Printf("There are %d even-ended numbers from %d to %d\n", count, start, end)
