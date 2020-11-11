9bfea616d09fd495f5ccb84ddb85b98339ad2c85
Zack Koppert <zkoppert@github.com>
2020-11-11 09:30:06 -0800

    Add counting of numbers
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- even-end/even-end.go
+++ even-end/even-end.go
@@ -6,13 +6,19 @@ import (
 
 func main() {
 	var i int
-	for i = 1; i <= 35; i++ {
+	var count int
+	start := 1000
+	end := 9999
+
+	for i = start; i <= end; i++ {
 		if isEvenEnd(i) {
-			fmt.Printf("%d is even-ended\n", i)
+			//fmt.Printf("%d is even-ended\n", i)
+			count++
 		} else {
-			fmt.Printf("%d is not even-ended\n", i)
+			//fmt.Printf("%d is not even-ended\n", i)
 		}
 	}
+	fmt.Printf("There are %d even-ended numbers from %d to %d\n", count, start, end)
 }
 
 func isEvenEnd(x int) bool {
