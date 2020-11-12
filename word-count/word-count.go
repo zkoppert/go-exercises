38e9d5430d6ce4a0128d853ffb80885b727f9696
Zack Koppert <zkoppert@github.com>
2020-11-12 11:53:07 -0800

    Add spaces into comment lines for readibility
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- word-count/word-count.go
+++ word-count/word-count.go
@@ -11,13 +11,13 @@ func main() {
 		"Sew me a sail " +
 		"To catch me the Wind "
 
-	//Account for case-insensitivity
+	// Account for case-insensitivity
 	textLowercase := strings.ToLower(text)
 
-	//Split text into words
+	// Split text into words
 	wordArray := strings.Fields(textLowercase)
 
-	//Iterate over words to get count
+	// Iterate over words to get count
 	wordMap := map[string]int{}
 	for _, word := range wordArray {
 		//store the word and it's count to wordMap
