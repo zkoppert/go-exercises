9f928af3c2cce64572cb881c7efefd503e9ed322
Zack Koppert <zkoppert@github.com>
2020-11-12 11:38:50 -0800

    simplify even-end code
    Signed-off-by: Zack Koppert <zkoppert@github.com>

--- even-end/even-end.go
+++ even-end/even-end.go
@@ -13,10 +13,8 @@ func main() {
 	for i = start; i <= end; i++ {
 		for j = i; j <= end; j++ {
 			if isEvenEnd(i * j) {
-				//fmt.Printf("%d is even-ended\n", i)
+				// fmt.Printf("%d is even-ended\n", i)
 				count++
-			} else {
-				//fmt.Printf("%d is not even-ended\n", i)
 			}
 		}
 	}
@@ -26,8 +24,5 @@ func main() {
 func isEvenEnd(x int) bool {
 	xString := fmt.Sprintf("%d", x)
 
-	if xString[0] == xString[len(xString)-1] {
-		return true
-	}
-	return false
+	return xString[0] == xString[len(xString)-1]
 }
